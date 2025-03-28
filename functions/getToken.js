43ec600e088d4c1012626d6b32b89f07df8d73d4
Theo <71170619+Theo1337@users.noreply.github.com>
2025-03-28 14:05:39 -0300

    removed secrets from function

--- functions/getToken.js
+++ functions/getToken.js
@@ -1,6 +1,6 @@
 const getToken = async () => {
-  const TWITCH_CLIENT_ID = 'g23nf3u0ej1o7jkzi04q93n735jawx';
-  const TWITCH_SECRET = 'ir8jymwkw9xlf0u9moj0hochqry2z0';
+  const TWITCH_CLIENT_ID = '[TWITCH_CLIENT_ID]';
+  const TWITCH_SECRET = '[TWITCH_SECRET]';
   const TOKEN_URL = `https://id.twitch.tv/oauth2/token?client_id=${TWITCH_CLIENT_ID}&client_secret=${TWITCH_SECRET}&grant_type=client_credentials`;
 
   const options = {
